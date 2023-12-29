fe13745ca74daaebe02bde995f9178d0433ab66c
hamed <92758067+Hamedsarkhany@users.noreply.github.com>
2023-12-29 03:41:56 +0330

    add dashboard

--- js/main.js
+++ js/main.js
@@ -24,22 +24,25 @@ function toggleMenu() {
 
 
 function  haederanime() {
-    var bodyElement = document.querySelector(".header");
-    var navElement = document.querySelector("header");
-    var dropdown = document.querySelector(".dropdown-content");
-    if (this.scrollY > 500) {
-        navElement.style.position = "fixed"
-        navElement.style.zIndex = 2
-        navElement.style.backgroundColor = "#0F1A27"
-        dropdown.style.backgroundColor = "#0F1A27"
-        bodyElement.style.margin = "1rem 0rem"
+    if (document.querySelector(".header")) {
+        var bodyElement = document.querySelector(".header");
+        var navElement = document.querySelector("header");
+        var dropdown = document.querySelector(".dropdown-content");
+        if (this.scrollY > 500) {
+            navElement.style.position = "fixed"
+            navElement.style.zIndex = 2
+            navElement.style.backgroundColor = "#0F1A27"
+            dropdown.style.backgroundColor = "#0F1A27"
+            bodyElement.style.margin = "1rem 0rem"
 
-    }else {
-        navElement.style.position = "absolute"
-        navElement.style.zIndex = 2
-        navElement.style.backgroundColor = "rgb(15 26 39 / 0%)"
-        dropdown.style.backgroundColor = "rgb(15 26 39 / 0%)"
+        } else {
+            navElement.style.position = "absolute"
+            navElement.style.zIndex = 2
+            navElement.style.backgroundColor = "rgb(15 26 39 / 0%)"
+            dropdown.style.backgroundColor = "rgb(15 26 39 / 0%)"
+        }
     }
+    
 }
 
 window.addEventListener("scroll", haederanime , false);
